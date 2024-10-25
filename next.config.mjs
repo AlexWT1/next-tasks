2b7f4764b108d231bfa31783d620d49eabf18d16
Lusfan <nevzlom.vtk@mail.ru>
2024-10-25 23:27:17 +0300

    1

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@
 const nextConfig = {
     images: {
         domains: ["localhost", "img.clerk.com"],
-    }
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
 };
 
 export default nextConfig;
